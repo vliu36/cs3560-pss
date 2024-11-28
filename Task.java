098e1ad996eb29457db506f17773aa979c69fdc0
vliu36 <116984041+vliu36@users.noreply.github.com>
2024-11-27 17:12:14 -0800

    completed tasks 3 and 5

--- Task.java
+++ Task.java
@@ -1,3 +1,67 @@
-public class Task {
+import Interfaces.TaskInterface;
+public class Task implements TaskInterface {
+    protected String name;
+    protected Type type;
+    protected double startTime;
+    protected double duration;
+    protected int startDate;
+
+    @Override
+    public TaskInterface newTask(String name, Type type, double start, double duration, int date) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'newTask'");
+    }
+    @Override
+    public double getStartTime() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getStartTime'");
+    }
+
+    @Override
+    public void setStartTime(double time) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setStartTime'");
+    }
+
+    @Override
+    public double getDuration() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDuration'");
+    }
+
+    @Override
+    public void setDuration(double time) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setDuration'");
+    }
+
+    @Override
+    public int getStartDate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getStartDate'");
+    }
+
+    @Override
+    public void setStartDate(int date) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setStartDate'");
+    }
+    @Override
+    public int getEndDate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEndDate'");
+    }
+
+    @Override
+    public double getEndTime() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEndTime'");
+    }
+
+    @Override
+    public void printTask() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'printTask'");
+    }
     
 }
