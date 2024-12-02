b3d3bae385e6a275661f79e82772aa15f0e02e8c
cons1187 <146302305+cons1187@users.noreply.github.com>
2024-12-01 18:22:52 -0800

    Update AntiInterface.java

--- Interfaces/AntiInterface.java
+++ Interfaces/AntiInterface.java
@@ -3,6 +3,6 @@ package Interfaces;
 public interface AntiInterface extends TaskInterface{
     //method checks for a recurring task that the anti task can override
     //if there is no recurring task that matches the start time and duration the anti task will not be created
-    public void overrideRecurring();
+    public boolean overrideRecurring();
 
 }
