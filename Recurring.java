ae94e30241bf410446d045a9b1113aadd55a0dd4
cons1187 <146302305+cons1187@users.noreply.github.com>
2024-12-01 18:41:48 -0800

    Update Recurring.java

--- Recurring.java
+++ Recurring.java
@@ -1,3 +1,19 @@
-public class Recurring {
+import Interfaces.RecurringInterface;
+
+public class Recurring extends Task {
+
+    private int frequency;
+
+    public Recurring(String name, String type, double start, double duration, int date, int frequency) {
+        super(name, type, start, duration, date);
+        this.frequency = frequency;
+    } 
     
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
 }
