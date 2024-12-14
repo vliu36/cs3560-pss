e7e9be682f5eb95c5b6ba8ca2fea7df24b2a8887
fjunqu <147464169+fjunqu@users.noreply.github.com>
2024-12-13 22:34:58 -0800

    Update Transient.java

--- Transient.java
+++ Transient.java
@@ -3,4 +3,7 @@ public class Transient extends Task {
     public Transient(String name, String type, double start, double duration, int date) {
         super(name, type, start, duration, date);
     }
+    public Transient(String name, Type type, double start, double duration, int date) {
+        super(name, type, start, duration, date);
+    }
 }
