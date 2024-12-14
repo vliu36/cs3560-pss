7d69bc5d42d1c468c178851752f4afd875471aa3
fjunqu <147464169+fjunqu@users.noreply.github.com>
2024-12-13 22:32:54 -0800

    Update Anti.java

--- Anti.java
+++ Anti.java
@@ -1,40 +1,14 @@
+import Interfaces.AntiInterface;
 import Interfaces.TaskInterface;
 
-public class Anti extends Task {
-    private Recurring associatedRecurringTask;
+public class Anti extends Task implements AntiInterface {
+    TaskInterface blockedTask;
 
-    public Anti(String name, String type, double start, double duration, int date, Recurring recurringTask) {
+    public Anti(String name, String type, double start, double duration, int date, TaskInterface blocked) {
         super(name, type, start, duration, date);
-        if (!isValidAntiTask(start, duration, date, recurringTask)) {
-            throw new IllegalArgumentException("Invalid Anti task: Does not match any occurrence of the recurring task.");
-        }
-        this.associatedRecurringTask = recurringTask;
+        this.blockedTask = blocked;
     }
-
-    public Recurring getAssociatedRecurringTask() {
-        return associatedRecurringTask;
-    }
-
-    public void setAssociatedRecurringTask(Recurring recurringTask) {
-        if (!isValidAntiTask(this.startTime, this.duration, this.startDate, recurringTask)) {
-            throw new IllegalArgumentException("Invalid Anti task: Does not match any occurrence of the recurring task.");
-        }
-        this.associatedRecurringTask = recurringTask;
-    }
-
-    private boolean isValidAntiTask(double start, double duration, int date, Recurring recurringTask) {
-        int taskDate = recurringTask.getStartDate();
-        while (taskDate <= recurringTask.getEndDate()) {
-            if (taskDate == date && recurringTask.getStartTime() == start && recurringTask.getDuration() == duration) {
-                return true; // Matches a valid instance
-            }
-            taskDate += recurringTask.getFrequency(); // Increment by frequency
-        }
-        return false; // No match found
-    }
-
-    @Override
-    public void printTask() {
-        System.out.println("Anti-Task: Cancels [" + associatedRecurringTask.getName() + "] on " + startDate);
+    public TaskInterface getBlockedTask() {
+        return blockedTask;
     }
 }
